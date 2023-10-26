92f178debbafbc8ada531a681497cc7ebba47c23
Tanush Golwala <tanushgolwala@gmail.com>
2023-10-27 02:12:12 +0530

    feat: added profile page

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
+import 'package:echo_link/screens/loginpage.dart';
+import 'package:echo_link/screens/profile.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const EchoLink());
 }
-
-
 
 class EchoLink extends StatefulWidget {
   const EchoLink({super.key});
@@ -16,6 +16,10 @@ class EchoLink extends StatefulWidget {
 class _EchoLinkState extends State<EchoLink> {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return const MaterialApp(
+      home: Scaffold(
+        body: Prof(),
+      ),
+    );
   }
 }
